543ab773004fe18e447851f3eeca1eadff211ff6
DarioFerrer <darioferrer143@gmail.com>
2024-01-20 11:22:58 -0300

    Shopping Cart

---
+++ 1vite-project/src/Context/index.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from 'react'
+
+export const ShoppingCartContext = createContext()
+
+export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart · Increment quantity
+    const [count, setCount] = useState(0)
+
+    // Product Detail · Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail · Show product
+    const [productToShow, setProductToShow] = useState({})
+
+    // Shopping Cart · Add products to cart
+    const [cartProducts, setCartProducts] = useState([])
+
+    return (
+        <ShoppingCartContext.Provider value={{
+            count,
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts
+        }}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
+
